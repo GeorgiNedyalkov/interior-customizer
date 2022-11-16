6c819d76659895a23c845f0613cd392a3aea24e8
Flu3nt <gnedyalkov94@gmail.com>
2022-11-16 13:20:56 +0200

    Create sidebar menu for categories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css"
-import Navbar from "./components/Navbar"
+import Navbar from "./components/navbar/Navbar"
+import Sidebar from "./components/sidebar/Sidebar"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="home">
+        <div className="displayer">
+          <img
+            className="display-img"
+            src="https://images.nabr.com/display?path=ConfigOptions/SOFA1/design_studio_default_carousel_image.webp&h=0&w=1200&op=resize"
+            alt=""
+          />
+        </div>
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,18 @@
 import "./Sidebar.css"
+import Button from "../button/Button"
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
+      <SidebarMenu />
+      <Button>See available homes</Button>
+    </div>
+  )
+}
+
+const SidebarMenu = () => {
+  return (
+    <div className="sidebar-menu">
       <ul className="categories">
         <li>Size</li>
         <li>Designer</li>
