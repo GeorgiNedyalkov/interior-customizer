6cf79a73133673e0a260e3f18cd303d80a6064f5
Flu3nt <gnedyalkov94@gmail.com>
2022-11-16 12:47:16 +0200

    Create new folder structure

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import "./Sidebar.css"
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul className="categories">
+        <li>Size</li>
+        <li>Designer</li>
+        <li>Features</li>
+        <li>Add-Ons</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
