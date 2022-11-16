03c9bbd0fa7e44050d8d5421a44a2fa877cb2d24
Flu3nt <gnedyalkov94@gmail.com>
2022-11-16 12:23:49 +0200

    Create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 className="nav-title">Custom Home</h1>
+    </div>
+  )
+}
+
+export default Navbar
