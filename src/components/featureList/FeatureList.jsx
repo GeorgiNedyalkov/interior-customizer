99ecfb01df0f719e2738ca3fd16b88aabc3602d5
Flu3nt <gnedyalkov94@gmail.com>
2022-12-02 13:44:55 +0200

    Extract featuresList to separate data file

--- src/components/featureList/FeatureList.jsx
+++ src/components/featureList/FeatureList.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import { AiOutlineDown, AiOutlineHeart } from "react-icons/ai";
-import { featuresList } from "../../data/data";
+import { featuresList } from "../../data/featuresList";
 import Button from "../button/Button";
 import "./FeatureList.css";
 
 const FeatureList = () => {
+  const [showFeature, setShowFeature] = useState({
+    outdoor: false,
+    design: false,
+    enterntainment: false,
+    health: false,
+    luxuries: false,
+  });
+
+  const handleChangeFeature = (category) => {
+    setShowFeature((prevState) => ({
+      ...prevState,
+      [category]: !prevState[category],
+    }));
+  };
+
   return (
     <div className="features">
       <h1 className="title">What are your preferred features?</h1>
@@ -18,7 +33,11 @@ const FeatureList = () => {
           const { id, icon, title } = feature;
 
           return (
-            <div className="feature-section" key={id}>
+            <div
+              className="feature-section"
+              key={id}
+              onClick={handleChangeFeature}
+            >
               <div className="fc-top">
                 <div className="fc-left">
                   {icon}
@@ -27,41 +46,31 @@ const FeatureList = () => {
                 <AiOutlineDown />
               </div>
 
-              <div className="fc-bottom"></div>
+              <div className="fc-bottom">
+                {/* SubFeature */}
+
+                {showFeature && (
+                  <div className="sub-feature">
+                    {feature.features.map((feature) => (
+                      <div
+                        className="sub-feature"
+                        onClick={() => console.log(feature.title)}
+                      >
+                        <h2 className="sb-title">
+                          {feature.title}
+                          <AiOutlineHeart className="heart-icon" />
+                        </h2>
+                        <p>{`$${feature.price.toLocaleString()}`}</p>
+                        <p style={{ color: "green" }}>Details</p>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
             </div>
           );
         })}
       </div>
-
-      {featuresList.map((feature) => (
-        <>
-          <div className="feature-section" key={feature.id}>
-            <div className="fc-top">
-              <div className="fc-left">
-                {feature.icon}
-                <h2 className="feature-title">{feature.title}</h2>
-              </div>
-              <AiOutlineDown />
-            </div>
-
-            <div className="fc-bottom">
-              {feature.features.map((feature) => (
-                <div
-                  className="sub-feature"
-                  onClick={() => console.log(feature.title)}
-                >
-                  <h2 className="sb-title">
-                    {feature.title}
-                    <AiOutlineHeart className="heart-icon" />
-                  </h2>
-                  <p>{`$${feature.price.toLocaleString()}`}</p>
-                  <p style={{ color: "green" }}>Details</p>
-                </div>
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
       <Button>Continue</Button>
     </div>
   );
