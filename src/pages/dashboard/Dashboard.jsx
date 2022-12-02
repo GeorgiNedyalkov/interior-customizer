bbcbd2d144424011c45538ec87f1e0a18fc9e7bd
Flu3nt <gnedyalkov94@gmail.com>
2022-12-02 13:06:48 +0200

    Add dashboard page

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,39 @@
+import "./Dashboard.css";
+import { Link } from "react-router-dom";
+import Button from "../../components/button/Button";
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="dashboard">
+        <Navbar />
+        <div className="bg-image"></div>
+
+        <div className="hero">
+          <div className="hero-greeting">
+            <h1 className="hero-title">Welcome User!</h1>
+            <p className="hero-heading-text spacing">Let's design your home</p>
+            <p className="spacing">
+              Enter your preferences and see all available homes that are a
+              match.
+            </p>
+            <Button>
+              <Link to="/" style={{ color: "inherit" }}>
+                Find a home
+              </Link>
+            </Button>
+          </div>
+          <hero className="img-wrap">
+            <div className="hero-image"></div>
+          </hero>
+        </div>
+
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
