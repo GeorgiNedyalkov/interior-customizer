5582c60020a18db163770062891848f2af6913d2
Flu3nt <gnedyalkov94@gmail.com>
2023-08-07 14:54:31 +0300

    refactor hero component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -6,36 +6,38 @@ import Navbar from "../../components/Navbar/Navbar";
 
 const Dashboard = () => {
     return (
-        <>
-            <div className="dashboard">
-                <Navbar />
-                <div className="bg-image"></div>
-
-                <div className="hero">
-                    <div className="hero-greeting">
-                        <h1 className="hero-title">Welcome User!</h1>
-                        <p className="hero-heading-text spacing">
-                            Let's design your home
-                        </p>
-                        <p className="spacing">
-                            Enter your preferences and see all available homes
-                            that are a match.
-                        </p>
-                        <Button>
-                            <Link to="/size" className="link">
-                                Find a home
-                            </Link>
-                        </Button>
-                    </div>
-                    <hero className="img-wrap">
-                        <div className="hero-image"></div>
-                    </hero>
-                </div>
-
-                <Footer />
-            </div>
-        </>
+        <div className="dashboard">
+            <Navbar />
+            <div className="bg-image" />
+            <Hero />
+            <Footer />
+        </div>
     );
 };
 
 export default Dashboard;
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero-greeting">
+                <h1 className="hero-title">Welcome User!</h1>
+                <p className="hero-heading-text spacing">
+                    Let's design your home
+                </p>
+                <p className="spacing">
+                    Enter your preferences and see all available homes that are
+                    a match.
+                </p>
+                <Button>
+                    <Link to="/size" className="link">
+                        Find a home
+                    </Link>
+                </Button>
+            </div>
+            <hero className="img-wrap">
+                <div className="hero-image" />
+            </hero>
+        </div>
+    );
+};
