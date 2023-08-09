131b7608c3ebc08f1b04f96e6253e05e48623999
Flu3nt <gnedyalkov94@gmail.com>
2023-08-09 10:25:06 +0300

    refactor restyle error page

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import Button from "../../components/Button/Button";
 import "./Error.css";
 
 const Error = () => {
@@ -7,9 +9,11 @@ const Error = () => {
         <>
             <Navbar />
             <div className="error">
-                <h1 className="title">404</h1>
-                <p>Sorry, Your page is not found...</p>
-                <p>Click on the logo to go back to dashboard.</p>
+                <h1 className="title">404 Page not Found</h1>
+                <p>Sorry, the page you are looking for cannot be found.</p>
+                <Link to="/">
+                    <Button>Go Back</Button>
+                </Link>
             </div>
         </>
     );
