a4f21a9b3a092f514b7ce5f4f22f2368bb89087c
Georgi Nedyalkov <51360924+GeorgiNedyalkov@users.noreply.github.com>
2023-08-09 13:44:04 +0300

    Delete src/components directory

--- src/components/Badge/Badge.jsx
+++
@@ -1,7 +0,0 @@
-import "./Badge.css";
-
-const Badge = ({ name }) => {
-    return <div className="badge">{name}</div>;
-};
-
-export default Badge;
--- src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import "./Button.css";
-
-const Button = ({ children }) => {
-    return <button className="btn">{children}</button>;
-};
-
-export default Button;
